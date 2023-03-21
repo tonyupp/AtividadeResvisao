dc60da6841017a73acd8d2898fc1b5c1f6ccec5a
Seu user nome <ts444968.@gmail.com>
2023-03-20 22:01:37 -0300

    adicionar arquivos

--- App.java
+++ App.java
@@ -2,6 +2,9 @@ package AtividadeRevisao;
 
 public class App {
     public static void main(String[] args) {
+        ExemploRevisao obj = new ExemploRevisao();
+        obj.exemplo2();
+
         
     }
 }
--- ExemploRevisao.java
+++ ExemploRevisao.java
@@ -1,5 +1,41 @@
 package AtividadeRevisao;
 
+import java.util.Scanner;
+
+import org.jcp.xml.dsig.internal.SignerOutputStream;
+
 public class ExemploRevisao {
-    
-}
+    Scanner sc = new Scanner(System.in);
+
+    public void exemplo1() {
+        // 1. LER O n
+        System.out.println("DIGITE O NUMERO");
+        double ndigitado = sc.nextDouble();
+        // 2.se n for >10
+        if (ndigitado > 10) {
+            System.out.println("É maior que 10!");
+        } else {// 3. senao >10
+            System.out.println("Não é maior que 10!");
+        }
+    }
+
+    public void exemplo2() {
+        // 1. quantidade de horas trabalhada
+        System.out.println(" quantidade de horas trabalhada mes");
+        int horasTrabalhada = sc.nextInt();
+        //2. 
+        System.out.println("valor da hora");
+        double valorHora = sc.nextDouble();
+        //3.
+        System.out.println("calculando quantidade de horas extra");
+       int horasExtra = horasTrabalhada-200;
+       //4.
+       double salarioTotal = horasTrabalhada*+horasExtra*valorHora*50/100;
+       //5.mostrar
+       System.out.println("O Salario Total é R$"+salarioTotal);
+        }
+        
+
+
+    }
+
